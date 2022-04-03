4be8ed7bebcccd1d9cbb4c9a2e52c5755df5c47f
shu yuanqi <1324073644@qq.com>
2022-04-03 19:54:58 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <MyHeader></MyHeader>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './pages/MyHeader.vue'
 
 export default {
   name: 'App',
   components: {
+      MyHeader,
     HelloWorld
   }
 }
---
+++ src/pages/MyHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="site-title">
+      <a href="https://www.xiaoyu77.xyz"
+         rel="home">
+        <img src="/favicon.ico"
+             alt="none" />
+      </a>
+    </h1>
+    <p class="site-description">欢迎光临呢</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
